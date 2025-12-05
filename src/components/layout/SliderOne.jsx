706f1172e61972623bdcb60c21fa2b795884405b
unknown <alif.web.bd@gmail.com>
2025-12-05 15:59:55 +0600

    last commit

--- src/components/layout/SliderOne.jsx
+++ src/components/layout/SliderOne.jsx
@@ -16,10 +16,9 @@ const NextArrow = () => (
   </div>
 );
 
-// Custom Prev Arrow
 const PrevArrow = () => (
   <div
-    className="swiper-button-prev-custom-1 absolute left-0 md:-left-4 top-1/2 -translate-y-1/2 cursor-pointer z-20"
+    className="swiper-button-prev-custom-1 absolute left-0 md:-left-4  top-1/2 -translate-y-1/2 cursor-pointer z-20"
   >
     <Image src="/images/prev.png" />
   </div>
@@ -27,8 +26,9 @@ const PrevArrow = () => (
 
 const SliderOne = () => {
   return (
-    <Container>
-      <div className="relative px-4  sm:px-3 md:px-0">
+    //
+    <Container> 
+      <div className="relative"> 
 
         <PrevArrow />
         <NextArrow />
@@ -41,27 +41,27 @@ const SliderOne = () => {
           }}
           loop={true}
           speed={500}
-          slidesPerView={7}
+          slidesPerView={6}
           spaceBetween={10}
-           breakpoints={{
-                        320: { slidesPerView: 2, spaceBetween: 10 },
-                        640: { slidesPerView: 3, spaceBetween: 20 },
-                        768: { slidesPerView: 4, spaceBetween: 30 },
-                        1024: { slidesPerView: 7, spaceBetween: 40 },
-                    }}
-          className="bg-[#20213A] font-semibold  text-[#FFFFFF] text-[14px] leading-[150%]  rounded-2xl"
+          breakpoints={{
+            320: { slidesPerView: 2, spaceBetween: 10 }, 
+            640: { slidesPerView: 3, spaceBetween: 20 },
+            768: { slidesPerView: 4, spaceBetween: 30 },
+            1024: { slidesPerView: 6, spaceBetween: 40 },
+          }}
+          className="bg-[#20213A] font-semibold text-[#FFFFFF] text-[14px] leading-[150%] rounded-2xl text-center  "
         >
-          <SwiperSlide className="py-2.5 px-6">Course Overview</SwiperSlide>
-          <SwiperSlide className="py-2.5 px-6">Course Structure</SwiperSlide>
-          <SwiperSlide className="py-2.5 px-6">Curriculum</SwiperSlide>
-          <SwiperSlide className="py-2.5 px-6">Tools</SwiperSlide>
-          <SwiperSlide className="py-2.5 px-6">Prerequisite</SwiperSlide>
-          <SwiperSlide className="py-2.5 px-6">Projects</SwiperSlide>
-          <SwiperSlide className="py-2.5 px-6">For Whom</SwiperSlide>
-          <SwiperSlide className="py-2.5 px-6">Questions</SwiperSlide>
-          <SwiperSlide className="py-2.5 px-6">Success Students</SwiperSlide>
-          <SwiperSlide className="py-2.5 px-6">Payment Method</SwiperSlide>
-          <SwiperSlide className="py-2.5 px-6">Course Overview</SwiperSlide>
+          <SwiperSlide className="py-4 px-4">Course Overview</SwiperSlide>
+          <SwiperSlide className="py-4 px-4">Course Structure</SwiperSlide>
+          <SwiperSlide className="py-4 px-4">Curriculum</SwiperSlide>
+          <SwiperSlide className="py-4 px-4">Tools</SwiperSlide>
+          <SwiperSlide className="py-4 px-4">Prerequisite</SwiperSlide>
+          <SwiperSlide className="py-4 px-4">Projects</SwiperSlide>
+          <SwiperSlide className="py-4 px-4">For Whom</SwiperSlide>
+          <SwiperSlide className="py-4 px-4">Questions</SwiperSlide>
+          <SwiperSlide className="py-4 px-4">Success Students</SwiperSlide>
+          <SwiperSlide className="py-4 px-4">Payment Method</SwiperSlide>
+          <SwiperSlide className="py-4 px-4">Course Overview</SwiperSlide>
         </Swiper>
 
       </div>
